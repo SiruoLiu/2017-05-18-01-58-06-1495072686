531ff7a9058f6dde203433cdeb111529edf47c51
SiruoLiu <1143398076@qq.com>
2017-05-18 20:59:12 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
+module.exports = function main(scoreStr) {
   // Write your cade here
+  var scores=scoreStr.split("");
+  var arrLength=scores.length;
+  var sum=0;
+  for(var i=0;i<arrLength;i++){    
+      sum+=parseInt(scores[i])       
+      }
+  return 0;
 };
